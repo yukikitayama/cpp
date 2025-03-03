17c8133d3043b3429c7e2ebb2b026acc652fe32b
Yuki Kitayama <yk2797@columbia.edu>
2025-03-03 05:35:17 -0800

    array to function

--- udemy-beginning-cpp-programming/11-section/main.cpp
+++ udemy-beginning-cpp-programming/11-section/main.cpp
@@ -48,33 +48,47 @@ using namespace std;
 //     cout << "Hello " << prefix + " " + name + " " + suffix << endl;
 // }
 
-void print(int);
-void print(double = 125.5);
-void print(string);
-void print(string, string);
-void print(vector<string>);
+// void print(int);
+// void print(double = 125.5);
+// void print(string);
+// void print(string, string);
+// void print(vector<string>);
 
-void print(int num) {
-    cout << "Printing int: " << num << endl;
-}
+// void print(int num) {
+//     cout << "Printing int: " << num << endl;
+// }
 
-void print(double num) {
-    cout << "Printing double: " << num << endl;
-}
+// void print(double num) {
+//     cout << "Printing double: " << num << endl;
+// }
 
-void print(string s) {
-    cout << "Printing string: " << s << endl;
-}
+// void print(string s) {
+//     cout << "Printing string: " << s << endl;
+// }
 
-void print(string s, string t) {
-    cout << "Printing 2 strings: " << s << " and " << t << endl;
-}
+// void print(string s, string t) {
+//     cout << "Printing 2 strings: " << s << " and " << t << endl;
+// }
 
-void print(vector<string> v) {
-    cout << "Printing vector of strings: ";
-    for (auto s : v)
-        cout << s + " ";
+// void print(vector<string> v) {
+//     cout << "Printing vector of strings: ";
+//     for (auto s : v)
+//         cout << s + " ";
+//     cout << endl;
+// }
+
+void print_array(const int arr[], size_t size);
+void set_array(int arr[], size_t size, int value);
+
+void print_array(const int arr[], size_t size) {
+    for (size_t i {0}; i < size; ++i)
+        cout << arr[i] << " ";
     cout << endl;
+}
+
+void set_array(int arr[], size_t size, int value) {
+    for (size_t i {0}; i < size; ++i)
+        arr[i] = value;
 }
 
 int main() {
@@ -132,17 +146,23 @@ int main() {
     // greeting("Frank Miller", "Dr.");
     // greeting("William Smith");
 
-    print();
-    print(100);
-    print('A');
-    print(100.0);
-    print(123.3F);
-    print("C-style string");
-    string s {"C++ string"};
-    print(s);
-    print("C-style string", s);
-    vector<string> three_stooges {"Larry", "Moe", "Curly"};
-    print(three_stooges);
+    // print();
+    // print(100);
+    // print('A');
+    // print(100.0);
+    // print(123.3F);
+    // print("C-style string");
+    // string s {"C++ string"};
+    // print(s);
+    // print("C-style string", s);
+    // vector<string> three_stooges {"Larry", "Moe", "Curly"};
+    // print(three_stooges);
+
+    int my_scores[] {100, 98, 90, 86, 84};
+    print_array(my_scores, 5);
+    set_array(my_scores, 5, 100);
+    print_array(my_scores, 5);
+    // print_array(my_scores, 5);
 
     return 0;
 }
