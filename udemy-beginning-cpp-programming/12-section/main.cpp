70f1f611b0f90d6b5e2159527fbd1af9c2e0284f
Yuki Kitayama <yk2797@columbia.edu>
2025-06-22 16:53:41 -0700

    finish section 12

--- udemy-beginning-cpp-programming/12-section/main.cpp
+++ udemy-beginning-cpp-programming/12-section/main.cpp
@@ -41,6 +41,27 @@ void display(const int *const array, size_t size) {
         cout << array[i] << " ";
 }
 
+void print(const int *const arr, size_t size) {
+    cout << "[ ";
+    for (size_t i {0}; i < size; ++i)
+        cout << arr[i] << " ";
+    cout << "]";
+    cout << endl;
+}
+
+int * apply_all(const int *const arr1, size_t size1, const int *const arr2, size_t size2) {
+    int *new_array {};
+    new_array = new int[size1 * size2];
+    int position {0};
+    for (size_t i {0}; i < size2; ++i) {
+        for (size_t j {0}; j < size1; ++j) {
+            new_array[position] = arr1[j] * arr2[i];
+            ++position;
+        }
+    }
+    return new_array;
+}
+
 int main() {
     // int num {10};
     // cout << "Value of num is: " << num << endl;
@@ -199,19 +220,34 @@ int main() {
     // cout << num << endl;
     // cout << ref << endl;
 
-    vector<string> stooges {"Larry", "Moe", "Curly"};
+    // vector<string> stooges {"Larry", "Moe", "Curly"};
     
-    for (auto str: stooges)
-        str = "Funny";
-    for (auto str: stooges)
-        cout << str << endl;
-    cout << endl;
+    // for (auto str: stooges)
+    //     str = "Funny";
+    // for (auto str: stooges)
+    //     cout << str << endl;
+    // cout << endl;
 
-    for (auto &str: stooges)
-        str = "Funny";
-    for (auto const &str: stooges)
-        cout << str << endl;
-    cout << endl;
+    // for (auto &str: stooges)
+    //     str = "Funny";
+    // for (auto const &str: stooges)
+    //     cout << str << endl;
+    // cout << endl;
+
+    const size_t array1_size {5};
+    const size_t array2_size {3};
+    int array1[] {1, 2, 3, 4, 5};
+    int array2[] {10, 20, 30};
+    cout << "Array 1: ";
+    print(array1, array1_size);
+    cout << "Array 2: ";
+    print(array2, array2_size);
+
+    int *results = apply_all(array1, array1_size, array2, array2_size);
+    constexpr size_t results_size {array1_size * array2_size};
+    cout << "Result: ";
+    print(results, results_size);
+    delete [] results;
 
     return 0;
 }
