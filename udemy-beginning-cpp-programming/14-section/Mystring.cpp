1f188b0949da4e0981e971d985345ceb04a3ea5e
Yuki Kitayama <yk2797@columbia.edu>
2025-07-18 17:39:57 -0700

    finish overloading operators

--- udemy-beginning-cpp-programming/14-section/Mystring.cpp
+++ udemy-beginning-cpp-programming/14-section/Mystring.cpp
@@ -156,3 +156,78 @@ std::istream &operator>>(std::istream &in, Mystring &rhs) {
     delete [] buff;
     return in;
 }
+
+// Equality
+bool Mystring::operator==(const Mystring &rhs) {
+    return (std::strcmp(str, rhs.str) == 0);
+}
+
+// Not equality
+bool Mystring::operator!=(const Mystring &rhs) {
+    return !(std::strcmp(str, rhs.str) == 0);
+}
+
+// Less than
+bool Mystring::operator<(const Mystring &rhs) {
+    return !(std::strcmp(str, rhs.str) < 0);
+}
+
+// Greater than
+bool Mystring::operator>(const Mystring &rhs) {
+    return !(std::strcmp(str, rhs.str) > 0);
+}
+
+// Make lowercase
+Mystring Mystring::operator-() const {
+    char *buff = new char[std::strlen(str) + 1];
+    std::strcpy(buff, str);
+    for (size_t i = 0; i < std::strlen(buff); i++)
+        buff[i] = std::tolower(buff[i]);
+    Mystring temp {buff};
+    delete [] buff;
+    return temp;
+}
+
+// Concatenate
+Mystring Mystring::operator+(const Mystring &rhs) const {
+    char *buff = new char [std::strlen(str) + std::strlen(rhs.str) + 1];
+    std::strcpy(buff, str);
+    std::strcat(buff, rhs.str);
+    Mystring temp {buff};
+    delete [] buff;
+    return temp;
+}
+
+// Concat and assign
+Mystring &Mystring::operator+=(const Mystring &rhs) {
+    *this = *this + rhs;
+    return *this;
+}
+
+// Repeat
+Mystring Mystring::operator*(int n) const {
+    Mystring temp;
+    for (int i = 1; i <= n; i++)
+        temp = temp + *this;
+    return temp;
+}
+
+// Repeat and assign
+Mystring &Mystring::operator*=(int n) {
+    *this = *this * n;
+    return *this;
+}
+
+// Pre-increment - make the string upper-case
+Mystring &Mystring::operator++() {
+    for (size_t i = 0; i < std::strlen(str); i++)
+        str[i] = std::toupper(str[i]);
+    return *this;
+}
+
+// Post-increment - make the string upper-case
+Mystring Mystring::operator++(int) {
+    Mystring temp (*this);
+    operator++();
+    return temp;
+}
--- udemy-beginning-cpp-programming/14-section/main.cpp
+++ udemy-beginning-cpp-programming/14-section/main.cpp
@@ -64,7 +64,23 @@ int main() {
     // cin >> larry >> moe >> curly;
     // cout << "The three stooges are " << larry << ", " << moe << ", and " << curly << endl;
 
-    
+    cout << boolalpha << endl;
+    Mystring a {"frank"};
+    Mystring b {"frank"};
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+    b = "george";
+    cout << (a == b) << endl;
+    cout << (a != b) << endl;
+    cout << (a < b) << endl;
+    cout << (a > b) << endl;
+    Mystring s1 {"FRANK"};
+    s1 = -s1;
+    cout << s1 << endl;
+    s1 = s1 + "*****";
+    cout << s1 << endl;
+    s1 += "-----";
+    cout << s1 << endl;
 
     return 0;
 }
