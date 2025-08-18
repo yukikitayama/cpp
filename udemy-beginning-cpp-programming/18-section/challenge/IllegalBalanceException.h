8e3a0912d88bfe103b552df59d89d623c81cfc06
Yuki Kitayama <yk2797@columbia.edu>
2025-08-18 08:04:17 -0700

    finish exception

--- udemy-beginning-cpp-programming/18-section/challenge/IllegalBalanceException.h
+++ udemy-beginning-cpp-programming/18-section/challenge/IllegalBalanceException.h
@@ -1,10 +1,13 @@
 #ifndef __ILLEGAL_BALANCE_EXCEPTION_H_
 #define __ILLEGAL_BALANCE_EXCEPTION_H_
 
-class IllegalBalanceException {
+class IllegalBalanceException: public std::exception {
 public:
-    IllegalBalanceException() {}
-    ~IllegalBalanceException() {}
+    IllegalBalanceException() noexcept = default;
+    ~IllegalBalanceException() = default;
+    virtual const char *what() const noexcept {
+        return "Illegal balance exception";
+    }
 };
 
 #endif // __ILLEGAL_BALANCE_EXCEPTION_H_
