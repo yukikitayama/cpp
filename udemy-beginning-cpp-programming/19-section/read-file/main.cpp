3708fa6d11ad873ae4afca406ed46bc0c507e8ce
Yuki Kitayama <yk2797@columbia.edu>
2025-09-01 16:22:13 -0700

    getline:

--- udemy-beginning-cpp-programming/19-section/read-file/main.cpp
+++ udemy-beginning-cpp-programming/19-section/read-file/main.cpp
@@ -19,22 +19,48 @@ int main() {
     // std::cout << total << std::endl;
     // in_file.close();
 
+    // std::ifstream in_file;
+    // int num;
+    // double total;
+    // std::string line;
+    // in_file.open("test2.txt");
+    // if (!in_file) {
+    //     std::cerr << "Problem opening file" << std::endl;
+    //     return 1;
+    // }
+    // // while (!in_file.eof()) {
+    // while (in_file >> line >> num >> total) {
+    //     std::cout << std::setw(10) << std::left << line
+    //             << std::setw(10) << num
+    //             << std::setw(10) << total
+    //             << std::endl;
+    // }
+    // in_file.close();
+
+    // std::ifstream in_file;
+    // in_file.open("./poem.txt");
+    // if (!in_file) {
+    //     std::cerr << "Problem opening file" << std::endl;
+    //     return 1;
+    // }
+    // std::string line {};
+    // while (std::getline(in_file, line)) {
+    //     std::cout << line << std::endl;
+    // }
+    // in_file.close();
+
     std::ifstream in_file;
-    int num;
-    double total;
-    std::string line;
-    in_file.open("test2.txt");
+    in_file.open("./poem.txt");
     if (!in_file) {
         std::cerr << "Problem opening file" << std::endl;
         return 1;
     }
-    // while (!in_file.eof()) {
-    while (in_file >> line >> num >> total) {
-        std::cout << std::setw(10) << std::left << line
-                << std::setw(10) << num
-                << std::setw(10) << total
-                << std::endl;
+    char c {};
+    while (in_file.get(c)) {
+        std::cout << c;
     }
+    std::cout << std::endl;
     in_file.close();
+
     return 0;
 }
